1d31c103116ee314515482b1f0b0b4528eebbcef
Jean Alves <jean.donizete.alves@gmail.com>
2022-07-04 19:42:48 -0300

    feat: building componentes - imperative design

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { noop } from "rxjs";
-import { map } from "rxjs/operators";
-import { createHttpObservable } from "../common/util";
 
 @Component({
   selector: "about",
@@ -11,17 +8,5 @@ import { createHttpObservable } from "../common/util";
 export class AboutComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {
-    const http$ = createHttpObservable("/api/courses");
-
-    const courses$ = http$.pipe(
-      map(res => Object.values(res['payload']))
-    );
-
-    courses$.subscribe(
-      (courses) => console.log(courses),
-      noop,
-      () => console.log("completed")
-    );
-  }
+  ngOnInit() {}
 }

872908e1c3605f89156fb58a28cc055852d74169
Jean Alves <jean.donizete.alves@gmail.com>
2022-07-07 17:31:35 -0300

    feat: forkjoin operator

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -22,7 +22,7 @@ import {
   throttle,
   throttleTime,
 } from "rxjs/operators";
-import { merge, fromEvent, Observable, concat, interval } from "rxjs";
+import { merge, fromEvent, Observable, concat, interval, forkJoin } from "rxjs";
 import { Lesson } from "../model/lesson";
 import { createHttpObservable } from "../common/util";
 import { debug, RxJsLoggingLevel, setRxJsLogginLevel } from "../common/debug";
@@ -45,24 +45,23 @@ export class CourseComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     this.courseId = this.route.snapshot.params["id"];
 
-    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`).pipe(
-      debug(RxJsLoggingLevel.INFO, 'course value'),
-    );
+    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
 
-    setRxJsLogginLevel(RxJsLoggingLevel.DEBUG);
+    const lessons$ = this.loadLessons();
+
+    forkJoin(this.course$, lessons$).pipe(
+      tap(([course, lessons]) => {
+
+        console.log('course', course);
+        console.log('lessons', lessons);
+      })
+    ).subscribe();
   }
 
   ngAfterViewInit() {
     fromEvent<any>(this.input.nativeElement, "keyup")
-      .pipe(
-        map((event) => event.target.value),
-        startWith(""),
-        debug(RxJsLoggingLevel.TRACE, 'search'),
-        debounceTime(400),
-        distinctUntilChanged(),
-        switchMap(search => this.loadLessons(search)),
-        debug(RxJsLoggingLevel.DEBUG, 'lessons value'),
-      );
+      .pipe(map((event) => event.target.value))
+      .subscribe(console.log);
   }
 
   loadLessons(search = ""): Observable<Lesson[]> {

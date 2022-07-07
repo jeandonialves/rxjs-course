354428e5fd602ff03a7ca4f9b2ae066285e22875
Jean Alves <jean.donizete.alves@gmail.com>
2022-07-07 19:14:55 -0300

    feat: behaviorsubject store

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
+import { fromPromise } from "rxjs/internal-compatibility";
 import { delay, map, retryWhen, shareReplay, tap } from "rxjs/operators";
 import { Course } from "../model/course";
 import { createHttpObservable } from "./util";
@@ -37,5 +38,28 @@ export class Store {
         courses.filter((course) => course.category === category)
       )
     );
+  }
+
+  saveCourse(courseId: number, changes: Partial<Course>): Observable<any> {
+    const courses = this.subject.getValue();
+
+    const courseIndex = courses.findIndex((course) => course.id === courseId);
+
+    const newCourses = courses.slice(0);
+
+    newCourses[courseIndex] = {
+      ...courses[courseIndex],
+      ...changes,
+    };
+
+    this.subject.next(newCourses);
+
+    return fromPromise(fetch(`/api/courses/${courseId}`, {
+      method: 'PUT',
+      body: JSON.stringify(changes),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }))
   }
 }
